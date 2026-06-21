1ffb009d6b4d380edef9f821e54d345b57397571
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-06-21 23:59:35 +0530

    connect render backend

--- Client/src/utils/axios.js
+++ Client/src/utils/axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
+    config.headers["Authorization"] = `Bearer ${token}`;
   }
-    return config;
-    }   );
-    export default api;
+  return config;
+});
+export default api;

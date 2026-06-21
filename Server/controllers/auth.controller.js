464ed9bd3044150ceb43704951b2dfa7651cf620
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-06-22 01:01:56 +0530

    add extra error logs

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -36,7 +36,11 @@ export const register = async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Internal server error" });
+    console.error("REGISTER ERROR:", error);
+
+    res.status(500).json({
+      message: error.message,
+    });
   }
 };
 export const login = async (req, res) => {
